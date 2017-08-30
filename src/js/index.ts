a9ff34fdd1d5dad147e6a1ed5512107642b5be86
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-08-30 21:56:18 +0900

    implement boot application index function

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,36 @@
+/**
+ * @fileOverview Entry point of the app.
+ * create root application state and ticker.
+ *
+ * @author Tatsuya Yamamoto
+ */
+import ApplicationState from "./fsm/ApplicationState";
+
+/**
+ * Root application state;
+ * @type {any}
+ */
+const rootState = new ApplicationState();
+
+/**
+ * Main game element.
+ *
+ * @type {HTMLElement|any}
+ */
+const mainElement: HTMLElement = document.getElementById('main');
+
+/**
+ * Initialize the application.
+ */
 function init() {
-    console.info("Welcome Toridori!")
+    console.info("Welcome Toridori!");
+    mainElement.appendChild(<Node>rootState.view);
+
+    rootState.onEnter();
+    rootState.ticker.add((deltaTime: number) => {
+        rootState.update(deltaTime);
+    });
 }
 
+// Fire init() on page loaded.
 window.addEventListener('load', init);
