d4c35cce2c5e0a1f2ebe0151f17db58f96fe859d
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-08-31 21:56:53 +0900

    replace onButtonClick logic and show game state view.

--- src/js/container/sprite/ClickableSprite.ts
+++ src/js/container/sprite/ClickableSprite.ts
@@ -1,45 +1,23 @@
 import {Sprite, Texture, interaction} from 'pixi.js';
 
 /**
- * @interface
- */
-export interface OnClickListener {
-    onClick(event: interaction.InteractionEvent): void;
-}
-
-/**
  * @class
  */
 abstract class ClickableSprite extends Sprite {
-    private _onClickListener: OnClickListener;
-
     public constructor(texture: Texture) {
         super(texture);
 
         this.anchor.set(0.5);
-        this.interactive = true;
-        this.on('pointerdown', this._handleOnPointerDown);
     }
 
     /**
      * Set to {@link _onClickListener}
      *
-     * @param listener
+     * @param fn
      */
-    public setOnClickListener(listener: OnClickListener) {
-        this._onClickListener = listener;
-    }
-
-    /**
-     * Event callback function for pointerdown.
-     *
-     * @param event
-     * @private
-     */
-    private _handleOnPointerDown = (event: interaction.InteractionEvent) => {
-        if (this._onClickListener) {
-            this._onClickListener.onClick(event);
-        }
+    public setOnClickListener(fn: (event: interaction.InteractionEvent) => void) {
+        this.interactive = true;
+        this.on('pointerdown', fn);
     }
 }
 
--- src/js/container/views/TopViewContainer.ts
+++ src/js/container/views/TopViewContainer.ts
@@ -1,5 +1,9 @@
+import {interaction} from 'pixi.js';
+
 import ViewContainer from "./ViewContainer";
 import GameStartButton from "../sprite/button/GameStartButton";
+import {Events} from "../../fsm/ApplicationState";
+import {dispatchEvent} from '../../fsm/EventUtils'
 
 class TopViewContainer extends ViewContainer {
     private _gameStartButton;
@@ -7,6 +11,7 @@ class TopViewContainer extends ViewContainer {
     constructor() {
         super();
         this._gameStartButton = new GameStartButton();
+        this._gameStartButton.setOnClickListener(this.onGameStartButtonClick);
         this.addChild(this._gameStartButton)
     }
 
@@ -32,6 +37,10 @@ class TopViewContainer extends ViewContainer {
      */
     protected onRendererSizeChange(newWidth: number, newheight: number): void {
         super.onRendererSizeChange(newWidth, newheight);
+    }
+
+    private onGameStartButtonClick = (event: interaction.InteractionEvent): void => {
+        dispatchEvent(Events.GAME_START_REQUEST);
     }
 }
 
