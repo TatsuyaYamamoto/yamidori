f6432f3664cb4cf7ae8ecb6616d51be79147996d
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-09-01 10:03:45 +0900

    implement count dount game state

--- src/js/fsm/game/CountGameState.ts
+++ src/js/fsm/game/CountGameState.ts
@@ -1,18 +1,50 @@
 import GameState from "./GameState";
+import {dispatchEvent} from '../EventUtils';
+import {Events} from '../view/GameViewState';
 
 class CountGameState implements GameState {
     public static TAG = "CountGameState";
+    private _initialTimeMillis: number;
 
-    update(elapsedTime: number): void {
+    private _isCountedOne: boolean = false;
+    private _isCountedTwo: boolean = false;
+    private _isCountedThree: boolean = false;
 
+    update(elapsedTimeMillis: number): void {
+        this._initialTimeMillis += elapsedTimeMillis;
+
+        if (1000 < this._initialTimeMillis && !this._isCountedThree) {
+            console.log("Count down, 3!");
+            this._isCountedThree = true;
+        }
+
+        if (2000 < this._initialTimeMillis && !this._isCountedTwo) {
+            console.log("Count down, 2!");
+            this._isCountedTwo = true;
+        }
+
+        if (3000 < this._initialTimeMillis && !this._isCountedOne) {
+            console.log("Count down, 1!");
+            this._isCountedOne = true;
+        }
+
+        if (this.isFinishedCounting()) {
+            console.log("Count down, done!");
+            dispatchEvent(Events.GAME_START);
+        }
     }
 
     onEnter(): void {
         console.log(`${CountGameState.TAG}@onEnter`);
+        this._initialTimeMillis = 0;
     }
 
     onExit(): void {
         console.log(`${CountGameState.TAG}@onExit`);
+    }
+
+    private isFinishedCounting(): boolean {
+        return 4000 < this._initialTimeMillis && this._isCountedOne && this._isCountedTwo && this._isCountedThree;
     }
 }
 
--- src/js/fsm/view/GameViewState.ts
+++ src/js/fsm/view/GameViewState.ts
@@ -4,6 +4,9 @@ import ViewState from "./ViewState";
 import GameViewContainer from "../../container/views/GameViewContainer";
 import StateMachine from "../internal/StateMachine";
 import {addEvents, removeEvents} from "../EventUtils";
+import CountGameState from "../game/CountGameState";
+import OverGameState from "../game/OverGameState";
+import PlayingGameState from "../game/PlayingGameState";
 
 export enum Events {
     COUNT_START = "GameViewState@COUNT_START",
@@ -13,25 +16,47 @@ export enum Events {
 
 class GameViewState implements ViewState {
     public static TAG = "GameViewState";
-    private _gameStateMachine: StateMachine;
     private _container: GameViewContainer;
 
-    update(elapsedTime: number): void {
+    private _gameStateMachine: StateMachine;
+    private _countGameState: CountGameState;
+    private _overGameState: OverGameState;
+    private _playingGameState: PlayingGameState;
 
+    update(elapsedTime: number): void {
+        this._gameStateMachine.update(elapsedTime);
     }
 
     onEnter(): void {
         console.log(`${GameViewState.TAG}@onEnter`);
         this._container = new GameViewContainer();
 
-        this._gameStateMachine = new StateMachine({});
+        this._countGameState = new CountGameState();
+        this._overGameState = new OverGameState();
+        this._playingGameState = new PlayingGameState();
 
-        addEvents({});
+        this._gameStateMachine = new StateMachine({
+            [CountGameState.TAG]: this._countGameState,
+            [OverGameState.TAG]: this._overGameState,
+            [PlayingGameState.TAG]: this._playingGameState
+        });
+
+        addEvents({
+            [Events.COUNT_START]: this._changeToCountState,
+            [Events.GAME_START]: this._changeToPlayingGameState,
+            [Events.GAME_OVER]: this._changeToOverGameState,
+        });
+
+        this._gameStateMachine.init(CountGameState.TAG);
     }
-    
+
     onExit(): void {
         console.log(`${GameViewState.TAG}@onExit`);
-        removeEvents([]);
+        removeEvents([
+            Events.COUNT_START,
+            Events.GAME_START,
+            Events.GAME_OVER
+        ]);
     }
 
     /**
@@ -43,6 +68,18 @@ class GameViewState implements ViewState {
      */
     public getContainer = (): Container => {
         return this._container;
+    };
+
+    private _changeToCountState = (): void => {
+
+    };
+
+    private _changeToPlayingGameState = (): void => {
+
+    };
+
+    private _changeToOverGameState = (): void => {
+
     }
 }
 
