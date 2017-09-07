b6ce026f29e7ef41e00bcf60188c07cdc467e621
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-09-07 20:55:21 +0900

    set load rotete kotori image

--- src/js/container/views/LoadViewContainer.ts
+++ src/js/container/views/LoadViewContainer.ts
@@ -2,6 +2,7 @@ import {Container} from 'pixi.js';
 
 import Text from "../sprite/text/Text";
 import BrandLogoAnimation from "../BrandLogoAnimation.js";
+import LoadKotoriAnimation from "../LoadKotoriAnimation.js";
 import {Events} from '../../fsm/view/LoadViewState'
 import {getCurrentViewSize} from "../../utils";
 import {dispatchEvent} from '../../fsm/EventUtils';
@@ -9,6 +10,7 @@ import {dispatchEvent} from '../../fsm/EventUtils';
 class LoadViewContainer extends Container {
     private _loadingInfoText;
     private _brandLogoAnimation: BrandLogoAnimation;
+    private _loadKotoriAnimation: LoadKotoriAnimation;
     private _loadedProgressPercentage: number = 0;
 
     constructor() {
@@ -16,15 +18,19 @@ class LoadViewContainer extends Container {
         const {width, height} = getCurrentViewSize();
 
         this._loadingInfoText = new Text(`Now loading... ${this._loadedProgressPercentage}%`);
-        this._loadingInfoText.x = 300;
-        this._loadingInfoText.y = 300;
+        this._loadingInfoText.position.set(width * 0.6, height * 0.8);
 
         this._brandLogoAnimation = new BrandLogoAnimation();
         this._brandLogoAnimation.position.set(width / 2, height / 2);
 
+        this._loadKotoriAnimation = new LoadKotoriAnimation(100);
+        this._loadKotoriAnimation.position.set(width * 0.2, height * 0.8);
+        this._loadKotoriAnimation.scale.set(0.2);
+
         this.addChild(
             this._loadingInfoText,
-            this._brandLogoAnimation
+            this._brandLogoAnimation,
+            this._loadKotoriAnimation,
         );
 
         this._brandLogoAnimation
@@ -35,6 +41,7 @@ class LoadViewContainer extends Container {
     public updateLoadedProgress(percentage: number): void {
         this._loadedProgressPercentage = percentage;
         this._loadingInfoText.text = `Now loading... ${Math.round(this._loadedProgressPercentage)}%`;
+        this._loadKotoriAnimation.progress(percentage);
     }
 }
 
