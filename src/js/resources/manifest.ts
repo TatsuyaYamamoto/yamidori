0188cd1e1bf8424b6b3e50ec8c3e21ed6bb29986
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-09-08 20:29:11 +0900

    WIP, register sound resource

--- src/js/resources/manifest.ts
+++ src/js/resources/manifest.ts
@@ -1,22 +1,25 @@
-const BASE_DIR = 'assets/image/';
+const IMAGE_BASE_DIR = 'assets/image/';
+const SOUND_BASE_DIR = 'assets/sound/';
 
 export default {
-    background: `${BASE_DIR}background.png`,
-    backgroundMenu: `${BASE_DIR}background_menu.png`,
+    background: `${IMAGE_BASE_DIR}background.png`,
+    backgroundMenu: `${IMAGE_BASE_DIR}background_menu.png`,
 
-    logoTitle: `${BASE_DIR}logo_title.png`,
-    logoGameOver: `${BASE_DIR}logo_gameover.png`,
+    logoTitle: `${IMAGE_BASE_DIR}logo_title.png`,
+    logoGameOver: `${IMAGE_BASE_DIR}logo_gameover.png`,
 
-    kotoriLeft: `${BASE_DIR}kotori_left.png`,
-    kotoriRight: `${BASE_DIR}kotori_right.png`,
+    kotoriLeft: `${IMAGE_BASE_DIR}kotori_left.png`,
+    kotoriRight: `${IMAGE_BASE_DIR}kotori_right.png`,
 
-    buttonGameStart: `${BASE_DIR}button_game_start.png`,
-    buttonGameRestart: `${BASE_DIR}button_game_restart.png`,
-    buttonGoBackHome: `${BASE_DIR}button_go_back_home.png`,
-    buttonGoHowToPlay: `${BASE_DIR}button_go_how_to_play.png`,
-    buttonGoCredit: `${BASE_DIR}button_go_credit.png`,
-    buttonGoRanking: `${BASE_DIR}button_go_ranking.png`,
-    buttonGoTwitterHome: `${BASE_DIR}button_go_twitter_home.png`,
-    buttonResultTweet: `${BASE_DIR}button_result_tweet.png`,
-    buttonSound: `${BASE_DIR}button_sound.png`,
+    buttonGameStart: `${IMAGE_BASE_DIR}button_game_start.png`,
+    buttonGameRestart: `${IMAGE_BASE_DIR}button_game_restart.png`,
+    buttonGoBackHome: `${IMAGE_BASE_DIR}button_go_back_home.png`,
+    buttonGoHowToPlay: `${IMAGE_BASE_DIR}button_go_how_to_play.png`,
+    buttonGoCredit: `${IMAGE_BASE_DIR}button_go_credit.png`,
+    buttonGoRanking: `${IMAGE_BASE_DIR}button_go_ranking.png`,
+    buttonGoTwitterHome: `${IMAGE_BASE_DIR}button_go_twitter_home.png`,
+    buttonResultTweet: `${IMAGE_BASE_DIR}button_result_tweet.png`,
+    buttonSound: `${IMAGE_BASE_DIR}button_sound.png`,
+
+    soundZenkai: `${SOUND_BASE_DIR}zenkai.mp3`,
 }
