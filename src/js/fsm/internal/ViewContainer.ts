02ecf76b84f3e6888d2c9ed639ac8beb3f950dd1
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-09-16 14:35:25 +0900

    delete useless functions

--- src/js/fsm/internal/ViewContainer.ts
+++ src/js/fsm/internal/ViewContainer.ts
@@ -47,35 +47,6 @@ abstract class ViewContainer extends Container {
     public get informationLayer(): Container {
         return this._informationLayer;
     }
-
-    /**
-     * Fire on opening the view.
-     */
-    protected onOpen(): void {
-        window.addEventListener('resize', this.onResize);
-
-    }
-
-    /**
-     * Fire on closing the view
-     */
-    protected onClose(): void {
-        window.removeEventListener('resize', this.onResize);
-    }
-
-    /**
-     * Fire on resizing window.
-     *
-     * @param newWidth
-     * @param newheight
-     */
-    protected onRendererSizeChange(newWidth: number, newheight: number): void {
-    }
-
-    private onResize = (event: Event): void => {
-        const {width, height} = getCurrentViewSize();
-        this.onRendererSizeChange(width, height);
-    }
 }
 
 export default ViewContainer;
