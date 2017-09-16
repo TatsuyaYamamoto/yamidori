5575fbeb3f196a505b1f3ab158d80a65de9a70d7
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-09-16 14:04:55 +0900

    WIP, concut view state and container

--- src/js/fsm/top/MenuTopState.ts
+++ src/js/fsm/top/MenuTopState.ts
@@ -1,6 +1,11 @@
 import {Container, interaction} from 'pixi.js';
 import Sound from "pixi-sound/lib/Sound";
 
+import State from "../internal/State";
+import {Events} from "../view/TopViewState";
+import {dispatchEvent} from '../EventUtils';
+
+import ViewSectionContainer from "../internal/ViewSectionContainer";
 import MenuBackground from "../../container/sprite/background/MenuBackground";
 import GameStartButton from "../../container/sprite/button/GameStartButton";
 import GoHowToPlayButton from "../../container/sprite/button/GoHowToPlayButton";
@@ -9,19 +14,15 @@ import GoCreditButton from "../../container/sprite/button/GoCreditButton";
 import GoTwitterHomeButton from "../../container/sprite/button/GoTwitterHomeButton";
 import SoundButton from "../../container/sprite/button/SoundButton";
 
-import {getCurrentViewSize} from "../../helper/utils";
-import {Events} from "../view/TopViewState";
-import {dispatchEvent} from '../EventUtils';
 import {goTo} from "../../helper/network";
 import {toggleMute} from "../../helper/SoundManager";
-import {URL} from '../../Constants';
 import {loadSound} from "../../helper/SoundManager";
 import manifest from '../../resources/manifest';
 
-class MenuTopState {
-    public static TAG = "MenuTopState";
+import {URL} from '../../Constants';
 
-    private _container: Container;
+class MenuTopState extends ViewSectionContainer implements State {
+    public static TAG = "MenuTopState";
 
     private _menuBackground: MenuBackground;
 
@@ -47,35 +48,32 @@ class MenuTopState {
     onEnter(): void {
         console.log(`${MenuTopState.TAG}@onEnter`);
 
-        const {width, height} = getCurrentViewSize();
-        this._container = new Container();
-
         this._menuBackground = new MenuBackground();
 
         this._gameStartButton = new GameStartButton();
-        this._gameStartButton.position.set(width * 0.2, height * 0.65);
+        this._gameStartButton.position.set(this.viewWidth * 0.2, this.viewHeight * 0.65);
         this._gameStartButton.setOnClickListener(this.onGameStartButtonClick);
 
         this._goHowToPlayButton = new GoHowToPlayButton();
-        this._goHowToPlayButton.position.set(width * 0.45, height * 0.65);
+        this._goHowToPlayButton.position.set(this.viewWidth * 0.45, this.viewHeight * 0.65);
         this._goHowToPlayButton.setOnClickListener(this.onHowToUseButtonClick);
 
         this._goRankingButton = new GoRankingButton();
-        this._goRankingButton.position.set(width * 0.7, height * 0.65);
+        this._goRankingButton.position.set(this.viewWidth * 0.7, this.viewHeight * 0.65);
 
         this._goCreditButton = new GoCreditButton();
-        this._goCreditButton.position.set(width * 0.9, height * 0.8);
+        this._goCreditButton.position.set(this.viewWidth * 0.9, this.viewHeight * 0.8);
         this._goCreditButton.setOnClickListener(this.onCreditButtonClick);
 
         this._goTwitterHomeButton = new GoTwitterHomeButton();
-        this._goTwitterHomeButton.position.set(width * 0.9, height * 0.15);
+        this._goTwitterHomeButton.position.set(this.viewWidth * 0.9, this.viewHeight * 0.15);
         this._goTwitterHomeButton.setOnClickListener(this.onTwitterHomeButtonClick);
 
         this._soundButton = new SoundButton();
-        this._soundButton.position.set(width * 0.8, height * 0.15);
+        this._soundButton.position.set(this.viewWidth * 0.8, this.viewHeight * 0.15);
         this._soundButton.setOnClickListener(this.onSoundButtonClick);
 
-        this._container.addChild(
+        this.addChild(
             this._menuBackground,
             this._gameStartButton,
             this._goCreditButton,
@@ -96,8 +94,12 @@ class MenuTopState {
         console.log(`${MenuTopState.TAG}@onExit`);
     }
 
+    /**
+     * @deprecated
+     * @return {any}
+     */
     public getContainer(): Container {
-        return this._container;
+        return this;
     }
 
     private onGameStartButtonClick = (event: interaction.InteractionEvent): void => {
