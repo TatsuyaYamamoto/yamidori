8a6fb50bfa70245e3f6894e75f01e58908b0bc07
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-09-19 00:01:05 +0900

    WIP, separate manifest files

---
+++ src/js/resources/image.ts
@@ -0,0 +1,28 @@
+/**
+ * @fileOverview Image resource manifest.
+ * It's used to load with {@link AssetLoader#setImageManifest}
+ */
+export default {
+    background: 'background.png',
+    backgroundMenu: 'background_menu.png',
+    backgroundCredit: 'background_credit.png',
+
+    logoTitle: 'logo_title.png',
+    logoGameOver: 'logo_gameover.png',
+    logoGameOverImage: 'logo_gameover_image.png',
+
+    kotoriLeft: 'kotori_left.png',
+    kotoriRight: 'kotori_right.png',
+
+    buttonGameStart: 'button_game_start.png',
+    buttonGameRestart: 'button_game_restart.png',
+    buttonGoBackHome: 'button_go_back_home.png',
+    buttonGoBackMenu: 'button_go_back_menu.png',
+    buttonGoHowToPlay: 'button_go_how_to_play.png',
+    buttonGoCredit: 'button_go_credit.png',
+    buttonGoRanking: 'button_go_ranking.png',
+    buttonGoTwitterHome: 'button_go_twitter_home.png',
+    buttonResultTweet: 'button_result_tweet.png',
+    buttonSound: 'button_sound.png',
+    buttonChangeLanguage: 'button_change_language.png',
+};
---
+++ src/js/resources/sound.ts
@@ -0,0 +1,15 @@
+/**
+ * @fileOverview Sound resource manifest.
+ * It's used to load with {@link AssetLoader#setSoundManifest}
+ */
+export default {
+    soundZenkai: 'zenkai.mp3',
+    soundGameLoop: 'game_loop.mp3',
+    soundGameEnd: 'game_end.mp3',
+    soundOk: 'ok.mp3',
+    soundCancel: 'cancel.mp3',
+    soundToggleSound: 'toggle_sound.mp3',
+    soundCountHigh: 'count_high.mp3',
+    soundCountLow: 'count_low.mp3',
+    soundTapKotori: 'tap_kotori.mp3',
+};
