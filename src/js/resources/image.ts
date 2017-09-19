19731609bd56c4cf4ce9ee9e4a03a17b9e920a63
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-09-20 01:00:24 +0900

    WIP, change image manifest structure

--- src/js/resources/image.ts
+++ src/js/resources/image.ts
@@ -2,27 +2,56 @@
  * @fileOverview Image resource manifest.
  * It's used to load with {@link AssetLoader#setImageManifest}
  */
+
+export const Ids = {
+    BACKGROUND: 'BACKGROUND',
+    BACKGROUND_MENU: 'BACKGROUND_MENU',
+    BACKGROUND_CREDIT: 'BACKGROUND_CREDIT',
+
+    LOGO_TITLE: 'LOGO_TITLE',
+    LOGO_GAMEOVER: 'LOGO_GAMEOVER',
+    LOGO_GAMEOVER_IMAGE: 'LOGO_GAMEOVER_IMAGE',
+
+    KOTORI_LEFT: 'KOTORI_LEFT',
+    KOTORI_RIGHT: 'KOTORI_RIGHT',
+
+    BUTTON_GAME_START: 'BUTTON_GAME_START',
+    BUTTON_GAME_RESTART: 'BUTTON_GAME_RESTART',
+    BUTTON_GO_BACK_HOME: 'BUTTON_GO_BACK_HOME',
+    BUTTON_GO_BACK_MENU: 'BUTTON_GO_BACK_MENU',
+    BUTTON_GO_HOW_TO_PLAY: 'BUTTON_GO_HOW_TO_PLAY',
+    BUTTON_GO_CREDIT: 'BUTTON_GO_CREDIT',
+    BUTTON_GO_RANKING: 'BUTTON_GO_RANKING',
+    BUTTON_GO_TWITTER_HOME: 'BUTTON_GO_TWITTER_HOME',
+    BUTTON_RESULT_TWEET: 'BUTTON_RESULT_TWEET',
+    BUTTON_SOUND: 'BUTTON_SOUND',
+    BUTTON_CHANGE_LANGUAGE: 'BUTTON_CHANGE_LANGUAGE',
+};
+
 export default {
-    background: 'background.png',
-    backgroundMenu: 'background_menu.png',
-    backgroundCredit: 'background_credit.png',
+    en: {
+        [Ids.BACKGROUND]: 'background.png',
+        [Ids.BACKGROUND_MENU]: 'background_menu.png',
+        [Ids.BACKGROUND_CREDIT]: 'background_credit.png',
 
-    logoTitle: 'logo_title.png',
-    logoGameOver: 'logo_gameover.png',
-    logoGameOverImage: 'logo_gameover_image.png',
+        [Ids.LOGO_TITLE]: 'logo_title.png',
+        [Ids.LOGO_GAMEOVER]: 'logo_gameover.png',
+        [Ids.LOGO_GAMEOVER_IMAGE]: 'logo_gameover_image.png',
 
-    kotoriLeft: 'kotori_left.png',
-    kotoriRight: 'kotori_right.png',
+        [Ids.KOTORI_LEFT]: 'kotori_left.png',
+        [Ids.KOTORI_RIGHT]: 'kotori_right.png',
 
-    buttonGameStart: 'button_game_start.png',
-    buttonGameRestart: 'button_game_restart.png',
-    buttonGoBackHome: 'button_go_back_home.png',
-    buttonGoBackMenu: 'button_go_back_menu.png',
-    buttonGoHowToPlay: 'button_go_how_to_play.png',
-    buttonGoCredit: 'button_go_credit.png',
-    buttonGoRanking: 'button_go_ranking.png',
-    buttonGoTwitterHome: 'button_go_twitter_home.png',
-    buttonResultTweet: 'button_result_tweet.png',
-    buttonSound: 'button_sound.png',
-    buttonChangeLanguage: 'button_change_language.png',
+        [Ids.BUTTON_GAME_START]: 'button_game_start.png',
+        [Ids.BUTTON_GAME_RESTART]: 'button_game_restart.png',
+        [Ids.BUTTON_GO_BACK_HOME]: 'button_go_back_home.png',
+        [Ids.BUTTON_GO_BACK_MENU]: 'button_go_back_menu.png',
+        [Ids.BUTTON_GO_HOW_TO_PLAY]: 'button_go_how_to_play.png',
+        [Ids.BUTTON_GO_CREDIT]: 'button_go_credit.png',
+        [Ids.BUTTON_GO_RANKING]: 'button_go_ranking.png',
+        [Ids.BUTTON_GO_TWITTER_HOME]: 'button_go_twitter_home.png',
+        [Ids.BUTTON_RESULT_TWEET]: 'button_result_tweet.png',
+        [Ids.BUTTON_SOUND]: 'button_sound.png',
+        [Ids.BUTTON_CHANGE_LANGUAGE]: 'button_change_language.png',
+    },
+    ja: {}
 };
